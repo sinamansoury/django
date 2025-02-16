11177c1aaab02f5c456d71afdb819dbcee9aa963
sina <sinamansoury@gmail.com>
2025-02-16 21:03:02 +0330

    add url

--- e_shop/urls.py
+++ e_shop/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path('', include('Account.urls')),
     path('dashboard/', include('User_panel.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
